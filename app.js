02fbad14a9ed4f20a9e4ea7b926734d1916ac747
Stephen Gladney <stephengladney@gmail.com>
2019-09-24 23:45:03 -0400

    send response when endpoints hit

--- app.js
+++ app.js
@@ -4,31 +4,34 @@ const slack = require("./slack");
 const team = require("./team");
 
 app
-  .get("/qa", () => {
+  .get("/qa", (req, res) => {
     slack.notify.readyForQA(
       "emailnotifications",
       "SL-13561",
       "New One Off --- insert template with attachments not showing name of attachment",
       "gladney"
     );
+    res.status(200).send("OK");
   })
-  .get("/r4r", () => {
+  .get("/r4r", (req, res) => {
     slack.notify.readyForReview(
       "emailnotifications",
       "SL-13561",
       "Implement attachments for Workflow email pane"
     );
+    res.status(200).send("OK");
   })
-  .get("/r4m", () => {
+  .get("/r4m", (req, res) => {
     slack.notify.readyForMerge(
       "emailnotifications",
       "SL-13561",
       "Implement attachments for Workflow email pane",
       "gladney"
     );
+    res.status(200).send("OK");
   })
-  .get("/jirahook", () => {
-    
+  .get("/jirahook", (req, res) => {
+    res.status(200).send("OK");
   })
 
   .listen(process.env.PORT || 5000, process.env.IP, () => {
