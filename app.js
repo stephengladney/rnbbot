6f92b0bb9d4a16ae134b8a8533d69962b50a0c0b
Stephen Gladney <stephen.gladney@salesloft.com>
2019-11-11 12:41:48 -0500

    log changelong so we can ignore sprint updates

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ let statusPoller = setInterval(() => {
 
 app
   .post("/jirahook", (req, res) => {
-    console.log(req.body.issue)
+    console.log(JSON.stringify(req.body.changelog))
     const cardData = {
       cardNumber: req.body.issue.key,
       cardTitle: req.body.issue.fields.summary,
