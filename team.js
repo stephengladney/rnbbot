0b359f6544e44add81ec7cd50ccaf279fb347e85
Stephen Gladney <stephen@ordernerd.com>
2020-01-25 10:25:08 -0500

    Add ephemeral messages and slack user ids

--- team.js
+++ team.js
@@ -1,32 +1,36 @@
 const slackChannel = "rnb-private"
 const teamName = "Rhythm and Blues"
 class TeamMember {
-  constructor({ firstName, lastName, email, slackHandle }) {
+  constructor({ firstName, lastName, email, slackHandle, slackId }) {
     this.firstName = firstName
     this.lastName = lastName
     this.email = email
     this.slackHandle = slackHandle
+    this.slackId = slackId
   }
 }
 const qaEngineer = new TeamMember({
   firstName: "Belinda",
   lastName: "Goodman",
   email: "belinda.goodman@salesloft.com",
-  slackHandle: "belinda.goodman"
+  slackHandle: "belinda.goodman",
+  slackId: "U6NGS5BN1"
 })
 
 const designer = new TeamMember({
   firstName: "Sam",
   lastName: "Solomon",
   email: "sam.solomon@salesloft.com",
-  slackHandle: "sam.solomon"
+  slackHandle: "sam.solomon",
+  slackId: "U4GRF0ZB2"
 })
 
 const productManager = new TeamMember({
   firstName: "Daniel",
   lastName: "Andrews",
   email: "daniel.andrews@salesloft.com",
-  slackHandle: "daniel"
+  slackHandle: "daniel",
+  slackId: "U0BGKRRNW"
 })
 
 const engineers = [
@@ -34,43 +38,43 @@ const engineers = [
     firstName: "Stephen",
     lastName: "Gladney",
     email: "stephen.gladney@salesloft.com",
-    slackHandle: "gladney"
+    slackHandle: "gladney",
+    slackId: "U0JFDH6DT"
   }),
   new TeamMember({
     firstName: "Ray",
     lastName: "Gesualdo",
     email: "ray.gesualdo@salesloft.com",
-    slackHandle: "Ray"
+    slackHandle: "Ray",
+    slackId: "UAFN8FU76"
   }),
   new TeamMember({
     firstName: "Adrianna",
     lastName: "Valdivia",
     email: "adrianna.valdivia@salesloft.com",
-    slackHandle: "adrianna"
+    slackHandle: "adrianna",
+    slackId: "UF9DXJ6KH"
   }),
   new TeamMember({
     firstName: "Taylor",
     lastName: "Martin",
     email: "taylor.martin@salesloft.com",
-    slackHandle: "tmart"
+    slackHandle: "tmart",
+    slackId: "UE149MWM7"
   }),
   new TeamMember({
     firstName: "Stephen",
     lastName: "Settle",
     email: "stephen.settle@salesloft.com",
-    slackHandle: "settle"
-  }),
-  new TeamMember({
-    firstName: "Matt",
-    lastName: "Brooke",
-    email: "matt.brooke@salesloft.com",
-    slackHandle: "matt.brooke"
+    slackHandle: "settle",
+    slackId: "U04NZTQSL"
   }),
   new TeamMember({
     firstName: "Patrick",
     lastName: "Hoydar",
     email: "patrick.hoydar@salesloft.com",
-    slackHandle: "Hodar"
+    slackHandle: "Hodar",
+    slackId: "U727JM1F0"
   })
 ]
 
