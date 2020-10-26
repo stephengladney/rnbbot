f38684a72d357908956b8666e78808b02885d7d2
Stephen Gladney <stephengladney@gmail.com>
2020-10-26 17:42:15 -0400

    dont use nodemon. rm slash commands

--- src/routes/slackbot/index.ts
+++ src/routes/slackbot/index.ts
@@ -1,15 +1,15 @@
 const router = require("express").Router()
-import { processSlashCommand } from "../../lib/slack/slash_commands"
+// import { processSlashCommand } from "../../lib/slack/slash_commands"
 import { stagnantCards } from "../../app"
 import { Handler } from "../api/handlers"
 
 const SlashCommandHandler: Handler = (req, res) => {
   const user = req.body.user_id
   const text = req.body.text
-  processSlashCommand({
-    text,
-    user,
-  })
+  // processSlashCommand({
+  //   text,
+  //   user,
+  // })
   res.status(200).send()
 }
 
